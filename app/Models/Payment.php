444eb12afd9e7679b079ac47bbc991acf7ed8bd4
Kadiri <kadiri.moohamed@gmail.com>
2026-02-25 12:43:34 +0000

    feat: add fillable fields and relations to Payment model

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -3,8 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Payment extends Model
 {
-    //
+    protected $fillable = [
+        'amount',
+        'user_id',      // le membre qui doit
+        'expense_id',
+        'is_paid',
+        'paid_at',
+    ];
+
+    //  RELATIONS
+
+    public function expense(): BelongsTo
+    {
+        return $this->belongsTo(Expense::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }

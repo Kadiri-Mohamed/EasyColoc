3d82f6f4866cefc6b36cd8ea31a820b603092deb
Kadiri <kadiri.moohamed@gmail.com>
2026-02-25 12:43:15 +0000

    feat: add fillable fields and relations to Expense model

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -3,8 +3,41 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Expense extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'description',
+        'amount',
+        'expense_date',
+        'notes',
+        'colocation_id',
+        'payer_id',      
+        'category_id',
+    ];
+
+    //  RELATIONS
+
+    public function colocation(): BelongsTo
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function payer(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'payer_id');
+    }
+
+    public function payments(): HasMany
+    {
+        return $this->hasMany(Payment::class);
+    }
 }

a63dc6a20c5bbba4fdb2b1f2f2e6869a79dcdf2d
Kadiri <kadiri.moohamed@gmail.com>
2026-02-25 12:43:39 +0000

    feat: add fillable fields and relations to Invitation model

--- app/Models/Invitation.php
+++ app/Models/Invitation.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Invitation extends Model
 {
-    //
+    protected $fillable = [
+        'email',
+        'token',
+        'status',
+        'colocation_id',
+        'accepted_at',
+        'rejected_at',
+    ];
+
+    //  RELATIONS
+   
+    public function colocation(): BelongsTo
+    {
+        return $this->belongsTo(Colocation::class);
+    }
 }

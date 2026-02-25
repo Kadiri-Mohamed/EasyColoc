d7389b91d19d499d2d1ed1d79bf2916b2e81e452
Kadiri <kadiri.moohamed@gmail.com>
2026-02-25 12:42:58 +0000

    feat: add fillable fields and relations to Category and Colocation models

--- app/Models/Colocation.php
+++ app/Models/Colocation.php
@@ -3,8 +3,46 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Colocation extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description',
+        'status',    
+        'category_id',
+    ];
+
+    //  RELATIONS
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function members(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'memberships')
+            ->using(Membership::class)
+            ->withPivot(['membership_role', 'joined_at', 'left_at'])
+            ->withTimestamps();
+    }
+
+    public function memberships(): HasMany
+    {
+        return $this->hasMany(Membership::class);
+    }
+
+    public function invitations(): HasMany
+    {
+        return $this->hasMany(Invitation::class);
+    }
+
+    public function expenses(): HasMany
+    {
+        return $this->hasMany(Expense::class);
+    }
 }

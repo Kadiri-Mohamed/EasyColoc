7ebb66f85431a3c93a8784310bc197ed1f858dd0
Kadiri <kadiri.moohamed@gmail.com>
2026-02-26 15:35:34 +0000

    fix: set table name in Membership model

--- app/Models/Membership.php
+++ app/Models/Membership.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class Membership extends Pivot
 {
+    protected $table = 'memberships';
     protected $fillable = [
         'user_id',
         'colocation_id',

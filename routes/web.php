ce3e1a8e778dc31d26e1a77941ed476fcaa2028f
Kadiri <kadiri.moohamed@gmail.com>
2026-02-26 15:38:14 +0000

    feat: add colocation routes with CRUD operations

--- routes/web.php
+++ routes/web.php
@@ -3,22 +3,26 @@
 use App\Http\Controllers\Admin\AdminDashboardController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\UserDashboardController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\ColocationController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-        Route::get('/dashboard', [UserDashboardController::class, 'index'])->name('dashboard');
-        Route::post('/logout', LogoutController::class)->name('logout');
-        Route::view('profile', 'profile')->name('profile');
-        // ─ Routes Admin ─
-        Route::middleware(['admin'])->name('admin.')->group(function () {
-            Route::get('/dashboard-admin', [AdminDashboardController::class, 'index'])->name('dashboard');
-            Route::get('/users/{user}/ban', [AdminDashboardController::class, 'ban'])->name('users.ban');
-            Route::get('/users/{user}/unban', [AdminDashboardController::class, 'unban'])->name('users.unban');
-        });
+    Route::get('/dashboard', [UserDashboardController::class, 'index'])->name('dashboard');
+
+    Route::post('/logout', LogoutController::class)->name('logout');
+    Route::view('profile', 'profile')->name('profile');
+
+    // ─ Routes Admin ─
+    Route::middleware(['admin'])->name('admin.')->group(function () {
+        Route::get('/dashboard-admin', [AdminDashboardController::class, 'index'])->name('dashboard');
+        Route::get('/users/{user}/ban', [AdminDashboardController::class, 'ban'])->name('users.ban');
+        Route::get('/users/{user}/unban', [AdminDashboardController::class, 'unban'])->name('users.unban');
+    });
+
+    Route::resource('colocation', ColocationController::class)->except(['index']);
+    Route::get('/colocations', [ColocationController::class, 'index'])->name('colocation.index');
 });
 
 

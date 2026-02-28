2339a0c4dff93d35813f17e58a44ca26a5c3667a
Kadiri <kadiri.moohamed@gmail.com>
2026-02-28 17:08:32 +0000

    feat: add CheckBanned middleware to handle banned users

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware(['auth', 'verified' , 'check.banned'])->group(function () {
     Route::get('/dashboard', [UserDashboardController::class, 'index'])->name('dashboard');
 
     Route::post('/logout', LogoutController::class)->name('logout');
